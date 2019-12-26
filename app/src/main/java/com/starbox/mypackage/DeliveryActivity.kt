91b1312e9b8c811ad143ee258cda5668179f7a33
whoStarBox <y2142144@gmail.com>
2019-12-26 12:58:43 +0900

    MainActivity 생성 DeliveryActivity 생성 WaybillActivity 생성

---
+++ app/src/main/java/com/starbox/mypackage/DeliveryActivity.kt
@@ -0,0 +1,12 @@
+package com.starbox.mypackage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeliveryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery)
+    }
+}
